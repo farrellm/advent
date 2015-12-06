8f17f9a56def70c76b80a7aac2544b41ef016378
Matthew Farrell <farrellm@alum.mit.edu>
2015-12-06 11:58:53 -0500

    clean up, add Day1 solutions

--- src/Day2.hs
+++ src/Day2.hs
@@ -2,11 +2,10 @@ module Day2 ( ) where
 
 import Data.List.Split
 
+input = readFile "input/day2.txt"
+
 sideAreas l w h = [l * w, w * h, h* l]
-
 area [l, w, h] = 2 * (sum $ sideAreas l w h) + (minimum $ sideAreas l w h)
-
-input = readFile "input/day2"
 
 toSides = map ((map read) . splitOn "x") . lines :: String -> [[Int]]
 
@@ -15,7 +14,6 @@ p1 = sum . map area . toSides <$> input
 
 sidePerims l w h = map (2*) [l+w, l+h, w+h]
 bow l w h = l * w * h
-
 ribbon [l, w, h] = (minimum $ sidePerims l w h) + bow l w h
 
 p2 = sum . map ribbon . toSides <$> input
