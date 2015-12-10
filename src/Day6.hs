40119ddddbde4eabfe089bc9c5b562aa9854da6e
Matthew Farrell <farrellm@alum.mit.edu>
2015-12-10 00:29:43 -0500

    day 6

--- src/Day6.hs
+++ src/Day6.hs
@@ -7,8 +7,11 @@ import qualified Data.Text as T
 -- import Data.List.Split
 -- import Control.Arrow
 import Text.ParserCombinators.Parsec hiding ((<|>))
--- import Data.Vector as V
-import Data.Vector.Unboxed.Mutable as M hiding (read)
+import qualified Data.Vector as V
+import Data.Vector as V ((!))
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as M
+import Control.Monad.Primitive
 
 input = readFile "input/day6.txt"
 
@@ -40,10 +43,69 @@ eol = char '\n'
 parseCmds :: String -> Either ParseError [Cmd]
 parseCmds = parse cmdFile "(unknown)"
 
-cmds = (either (const []) id . parseCmds . T.unpack) <$> input
+applyRect :: (PrimMonad m, Unbox a) =>
+             (a -> a) ->
+             Vector (M.MVector (PrimState m) a) -> (Int, Int) -> (Int, Int) ->
+             m ()
+applyRect f grid (x1, y1) (x2, y2) =
+  forM_ [y1 .. y2] applyRow
+  where applyRow y =
+          forM_ [x1 .. x2] (applyCell $ grid ! y)
+        applyCell row x = do
+          v <- M.read row x
+          M.write row x (f v)
 
-size = 5 :: Int
+cmdRect :: (PrimMonad m) =>
+             Instr ->
+             Vector (M.MVector (PrimState m) Bool) -> (Int, Int) -> (Int, Int) ->
+             m ()
+cmdRect (Toggle) = applyRect not
+cmdRect (On) = applyRect (const True)
+cmdRect (Off) = applyRect (const False)
 
--- grid = M.replicateM size (M.replicate size False)
+p1 :: IO Int
+p1 = do
+  let size = 1000
 
--- gridRow = M.replicate size False
+  cmds <- (either (const []) id . parseCmds . T.unpack) <$> input
+
+  row <- M.replicate size False
+  grid <- V.replicateM size (M.clone row)
+
+  forM_ cmds (applyCmd grid)
+
+  countGrid grid
+
+  where applyCmd grid (Cmd ins q1 q2) = cmdRect ins grid q1 q2
+        countGrid grid = V.sum <$> V.mapM countRow grid
+        countRow row =
+          sum <$> mapM ((pure . boolToInt) <=< M.read row) [0..(M.length row - 1)]
+        boolToInt True = 1
+        boolToInt False = 0
+-- 543903
+
+cmdRect2 :: (PrimMonad m) =>
+             Instr ->
+             Vector (M.MVector (PrimState m) Int) -> (Int, Int) -> (Int, Int) ->
+             m ()
+cmdRect2 (Toggle) = applyRect (+2)
+cmdRect2 (On) = applyRect (+1)
+cmdRect2 (Off) = applyRect $ max 0 . (+ (-1))
+
+p2 :: IO Int
+p2 = do
+  let size = 1000
+
+  cmds <- (either (const []) id . parseCmds . T.unpack) <$> input
+
+  row <- M.replicate size 0
+  grid <- V.replicateM size (M.clone row)
+
+  forM_ cmds (applyCmd grid)
+
+  sumGrid grid
+
+  where applyCmd grid (Cmd ins q1 q2) = cmdRect2 ins grid q1 q2
+        sumGrid grid = V.sum <$> V.mapM sumRow grid
+        sumRow row = sum <$> mapM (M.read row) [0..(M.length row - 1)]
+-- 14687245
