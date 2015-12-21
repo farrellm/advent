09242caad74237f1bf1441d5383ed53942eb1352
Matthew Farrell <farrellm@alum.mit.edu>
2015-12-21 15:11:15 -0500

    add template

---
+++ src/Template.hs
@@ -0,0 +1,27 @@
+module Day8 (p1, p2) where
+
+import Control.Arrow
+import Control.Monad
+
+import Data.Char
+import Data.List
+import qualified Data.Map as M
+import Data.Map ((!))
+
+import Text.ParserCombinators.Parsec hiding ((<|>), State)
+import qualified Text.Parsec.Token as P
+import Text.Parsec.Language (emptyDef)
+
+lexer      = P.makeTokenParser emptyDef
+decimal    = P.decimal lexer
+identifier = P.identifier lexer
+whiteSpace = P.whiteSpace lexer
+
+input = readFile "input/day9.txt"
+
+test :: IO String
+test = pure ""
+
+p1 = 0
+
+p2 = 0
