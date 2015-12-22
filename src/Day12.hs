4aa0fe5e1ee82cbdb823f02a6678df45e473e4ea
Matthew Farrell <farrellm23@gmail.com>
2015-12-22 10:46:03 -0500

    finish day 12

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,32 +1,33 @@
-{-# LANGUAGE DeriveGeneric, OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Day12 (p1, p2) where
 
-import Control.Arrow
-import Control.Monad
-
-import Data.Char
-import Data.List
-import qualified Data.Map as M
-import Data.Map ((!))
-import qualified Data.Set as S
-import qualified Data.Text as T
-
 import qualified Data.ByteString.Lazy.Char8 as B
 import Data.Aeson
+import Data.Foldable
 import Data.Maybe
-import GHC.Generics
 import qualified Data.HashMap.Strict as H
+import Data.Scientific as Sci
+
+import Debug.Trace
 
 input = readFile "input/day12.txt"
 
-test :: IO String
-test = pure ""
+numbers :: Value -> [Scientific]
+numbers (Object o) = H.elems o >>= numbers
+numbers (Array a) = toList a >>= numbers
+numbers (String _) = []
+numbers (Number n) = [n]
+numbers x = trace (show x) undefined
 
-numbers :: Value -> [Integer]
-numbers obj = []
+p1 = (sum . numbers . Object) <$> ((fromJust . decode . B.pack) <$> input :: IO Object)
+-- 119433.0
 
-p1 = (H.keys) <$> ((fromJust . decode . B.pack) <$> input :: IO Object)
--- p1 = 0
+numbers2 (Object o)
+  | "red" `elem` (H.elems o) = []
+  | otherwise = H.elems o >>= numbers2
+numbers2 (Array a) = toList a >>= numbers2
+numbers2 o = numbers o
 
-p2 = 0
+p2 = (sum . numbers2 . Object) <$> ((fromJust . decode . B.pack) <$> input :: IO Object)
+-- 68466.0
